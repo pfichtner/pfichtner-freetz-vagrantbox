b96e4dac66dcf3e9ac7d7c62e82041122684dd90
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2023-01-14 17:53:03 +0100

    support for calling internal functions of freetz-menu

--- menu.sh
+++ menu.sh
@@ -29,24 +29,8 @@ pressAnyKey() {
 	echo; read -n 1 -p "Press Enter to continue..." && clear
 }
 
-configSubMenu() {
-	DOCKER_PULL="Update internal build system"
-	SET_PASSWD="Set password for $USERNAME"
-	NO_AUTOSTART="Disable script autostart"
-	YES_AUTOSTART="Enable script autostart"
-	NO_AUTOLOGIN="Disable autologin"
-	YES_AUTOLOGIN="Enable autologin"
-
 hasTextBlock() {
 	[ -r "$1" ] && grep -Fq "$2" "$1"
-}
-
-isAutoLoginEnabled() {
-	if [ -r "/etc/inittab" ]; then
-		grep -q "agetty --skip-login --nonewline --noissue --autologin $USERNAME --noclear" /etc/inittab
-	else
-		hasTextBlock "$AUTOLOGIN_FILE" "$(autoLoginTextBlock)"
-	fi
 }
 
 autoLoginTextBlock() {
@@ -58,104 +42,147 @@ $LINE
 EOF
 }
 
-while :; do
-	value=()
-	value+=("$DOCKER_PULL" "Checks docker hub for update of the internal docker image.")
-	# [ "$(sudo passwd --status $USERNAME | cut -d' ' -f2)" = 'NP' ] && value+=("$SET_PASSWD" "$USERNAME has no password, you can set one.")
-	value+=("$SET_PASSWD" "set or update the password for $USERNAME.")
-	([ -r "$AUTOSTART_FILE" ] && grep -q "^\[ -x $THIS_FILE \] && $THIS_FILE$" "$AUTOSTART_FILE") && value+=("$NO_AUTOSTART" "Disables this script's autostart on login.") || value+=("$YES_AUTOSTART" "Enables this script's autostart on login.")
-	isAutoLoginEnabled && value+=("$NO_AUTOLOGIN" "Disables the autologin on virtual console 1.") || value+=("$YES_AUTOLOGIN" "Enables the autologin on virtual console 1.")
-	CHOICE=$(whiptail --title "Freetz-NG build config menu" --menu "Build Config Menu" 15 98 6 "${value[@]}" 3>&1 1>&2 2>&3)
-	[ "$?" -eq 0 ] || return
+isAutoLoginEnabled() {
+	if [ -r "/etc/inittab" ]; then
+		grep -q "agetty --skip-login --nonewline --noissue --autologin $USERNAME --noclear" /etc/inittab
+	else
+		hasTextBlock "$AUTOLOGIN_FILE" "$(autoLoginTextBlock)"
+	fi
+}
 
-	case $CHOICE in
-		"$DOCKER_PULL")
-			docker pull "$IMAGE"
-			pressAnyKey
-		;;
-		"$SET_PASSWD")
-		PASSWORD1=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
-		[ "$?" == 0 ] && PASSWORD2=$(whiptail --passwordbox "please reenter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
-		[ "$?" == 0 ] && sudo passwd "$USERNAME" << EOD
-${PASSWORD1}
-${PASSWORD2}
-EOD
-		;;
-		"$YES_AUTOLOGIN")
-			if [ -r "/etc/inittab" ]; then
-				sudo sed -i '/^tty1::respawn:\/sbin\/getty 38400 tty1$/s/getty/agetty --skip-login --nonewline --noissue --autologin '$USERNAME' --noclear/' /etc/inittab
-			else
-				sudo mkdir -p $(dirname "$AUTOLOGIN_FILE")
-				MULTILINE_STRING=$(autoLoginTextBlock)
-				echo "$MULTILINE_STRING" | sudo tee -a "$AUTOLOGIN_FILE" >/dev/null
-			fi
-		;;
-		"$NO_AUTOLOGIN")
-			if [ -r "/etc/inittab" ]; then
-				sudo sed -i '/^tty1::respawn:\/sbin\/agetty --skip-login --nonewline --noissue --autologin '$USERNAME' --noclear 38400 tty1$/s/agetty --skip-login --nonewline --noissue --autologin builduser --noclear/getty/' /etc/inittab
-			else
-				REMAINING=$(awk 'FNR==NR{a[$0];next} !($0 in a)' <(autoLoginTextBlock) <(cat "$AUTOLOGIN_FILE"))
-				[ -n "$REMAINING" ] && echo "$REMAINING" | sudo tee "$AUTOLOGIN_FILE" >/dev/null || (sudo rm "$AUTOLOGIN_FILE" && sudo rmdir --ignore-fail-on-non-empty -p $(dirname "$AUTOLOGIN_FILE"))
-			fi
-		;;
-                "$YES_AUTOSTART")
-			echo "[ -x $THIS_FILE ] && $THIS_FILE" | sudo tee -a "$AUTOSTART_FILE" >/dev/null
-                ;;
-                "$NO_AUTOSTART")
-                       sudo sed -i "\|^\[ -x $THIS_FILE \] && $THIS_FILE$|d" "$AUTOSTART_FILE" && [ ! -s "$AUTOSTART_FILE" ]  && sudo rm "$AUTOSTART_FILE"
-                ;;
+enableAutoLogin() {
+    isAutoLoginEnabled && return
+    if [ -r "/etc/inittab" ]; then
+	    sudo sed -i '/^tty1::respawn:\/sbin\/getty 38400 tty1$/s/getty/agetty --skip-login --nonewline --noissue --autologin '$USERNAME' --noclear/' /etc/inittab
+    else
+	    sudo mkdir -p $(dirname "$AUTOLOGIN_FILE")
+	    MULTILINE_STRING=$(autoLoginTextBlock)
+	    echo "$MULTILINE_STRING" | sudo tee -a "$AUTOLOGIN_FILE" >/dev/null
+    fi
+}
 
-	esac
-done
+disableAutoLogin() {
+    isAutoLoginEnabled || return
+    if [ -r "/etc/inittab" ]; then
+	    sudo sed -i '/^tty1::respawn:\/sbin\/agetty --skip-login --nonewline --noissue --autologin '$USERNAME' --noclear 38400 tty1$/s/agetty --skip-login --nonewline --noissue --autologin builduser --noclear/getty/' /etc/inittab
+    else
+	    REMAINING=$(awk 'FNR==NR{a[$0];next} !($0 in a)' <(autoLoginTextBlock) <(cat "$AUTOLOGIN_FILE"))
+	    [ -n "$REMAINING" ] && echo "$REMAINING" | sudo tee "$AUTOLOGIN_FILE" >/dev/null || (sudo rm "$AUTOLOGIN_FILE" && sudo rmdir --ignore-fail-on-non-empty -p $(dirname "$AUTOLOGIN_FILE"))
+    fi
+}
+
+isAutostartEnabled() {
+    ([ -r "$AUTOSTART_FILE" ] && grep -q "^\[ -x $THIS_FILE \] && $THIS_FILE$" "$AUTOSTART_FILE")
+}
+
+enableAutostart() {
+    isAutostartEnabled || echo "[ -x $THIS_FILE ] && $THIS_FILE" | sudo tee -a "$AUTOSTART_FILE" >/dev/null
+    [ -r "$AUTOSTART_FILE" ] && chown builduser "$AUTOSTART_FILE"
+}
+
+disableAutostart() {
+    isAutostartEnabled && sudo sed -i "\|^\[ -x $THIS_FILE \] && $THIS_FILE$|d" "$AUTOSTART_FILE" && [ ! -s "$AUTOSTART_FILE" ]  && sudo rm "$AUTOSTART_FILE"
 }
 
 
+configSubMenu() {
+	DOCKER_PULL="Update internal build system"
+	SET_PASSWD="Set password for $USERNAME"
+	NO_AUTOSTART="Disable script autostart"
+	YES_AUTOSTART="Enable script autostart"
+	NO_AUTOLOGIN="Disable autologin"
+	YES_AUTOLOGIN="Enable autologin"
+	while :; do
+	    value=()
+	    value+=("$DOCKER_PULL" "Checks docker hub for update of the internal docker image.")
+	    # [ "$(sudo passwd --status $USERNAME | cut -d' ' -f2)" = 'NP' ] && value+=("$SET_PASSWD" "$USERNAME has no password, you can set one.")
+	    value+=("$SET_PASSWD" "set or update the password for $USERNAME.")
+	    isAutostartEnabled && value+=("$NO_AUTOSTART" "Disables this script's autostart on login.") || value+=("$YES_AUTOSTART" "Enables this script's autostart on login.")
+	    isAutoLoginEnabled && value+=("$NO_AUTOLOGIN" "Disables the autologin on virtual console 1.") || value+=("$YES_AUTOLOGIN" "Enables the autologin on virtual console 1.")
+	    CHOICE=$(whiptail --title "Freetz-NG build config menu" --menu "Build Config Menu" 15 98 6 "${value[@]}" 3>&1 1>&2 2>&3)
+	    [ "$?" -eq 0 ] || return
 
-while :; do
-	value=()
-	[ -d "$LOCAL_REPO" ] || value+=("$CLONE_REPO" "Creates a new clone of the Freetz-NG github repository.")
-	[ -d "$LOCAL_REPO" ] && value+=("$PULL_REPO" "Updates the local Freetz-NG clone.")
-	[ -d "$LOCAL_REPO" ] && value+=("$MAKE_CONFIG" "Configure the firmware (\"make menuconfig\").")
-	[ -r "$LOCAL_REPO/.config" ] && value+=("$MAKE" "Build the firmware (\"make\").")
-	[ -d "$LOCAL_REPO" ] && value+=("$MAKE_CLEAN" "Remove unpacked images and some cache files (\"make clean\").")
-	[ -d "$LOCAL_REPO" ] && value+=("$MAKE_DISTCLEAN" "Clean everything except the download directory (\"make distclean\").")
-	value+=("$CONFIGURATION" "Config/Tweak this tool.")
+	    case $CHOICE in
+		    "$DOCKER_PULL")
+			    docker pull "$IMAGE"
+			    pressAnyKey
+		    ;;
+		    "$SET_PASSWD")
+		    PASSWORD1=$(whiptail --passwordbox "please enter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+		    [ "$?" == 0 ] && PASSWORD2=$(whiptail --passwordbox "please reenter your secret password" 8 78 --title "password dialog" 3>&1 1>&2 2>&3)
+		    [ "$?" == 0 ] && sudo passwd "$USERNAME" << EOD
+${PASSWORD1}
+${PASSWORD2}
+EOD
+		    ;;
+		    "$YES_AUTOLOGIN")
+			    enableAutoLogin
+		    ;;
+		    "$NO_AUTOLOGIN")
+			    disableAutoLogin
+		    ;;
+		    "$YES_AUTOSTART")
+			    enableAutostart
+		    ;;
+		    "$NO_AUTOSTART")
+			    disableAutostart
+		    ;;
 
-	CHOICE=$(whiptail --title "Freetz-NG build menu" --menu "Main Menu" 15 98 6 "${value[@]}" 3>&1 1>&2 2>&3)
-	[ "$?" -eq 0 ] || exit
+	    esac
+    done
+}
 
-	case $CHOICE in
-		"$CLONE_REPO")
-			run-in-docker git clone "$GH_REPO" "$LOCAL_REPO"
-			pressAnyKey
-		;;
-		"$PULL_REPO")
-			(cd "$LOCAL_REPO" && git pull)
-			pressAnyKey
-		;;
-		"$MAKE_CONFIG")
-			(cd "$LOCAL_REPO" && run-in-docker make menuconfig)
-		;;
-		"$MAKE")
-			(cd "$LOCAL_REPO" && run-in-docker make)
-			pressAnyKey
-		;;
-		"$MAKE_CLEAN")
-			(cd "$LOCAL_REPO" && run-in-docker make clean)
-			pressAnyKey
-		;;
-		"$MAKE_DISTCLEAN")
-			(cd "$LOCAL_REPO" && run-in-docker make distclean)
-			pressAnyKey
-		;;
-		"$DOCKER_PULL")
-			docker pull "$IMAGE"
-			pressAnyKey
+mainChoice() {
+    case $1 in
+	    "$CLONE_REPO")
+		    run-in-docker git clone "$GH_REPO" "$LOCAL_REPO"
+		    pressAnyKey
+	    ;;
+	    "$PULL_REPO")
+		    (cd "$LOCAL_REPO" && git pull)
+		    pressAnyKey
+	    ;;
+	    "$MAKE_CONFIG")
+		    (cd "$LOCAL_REPO" && run-in-docker make menuconfig)
+	    ;;
+	    "$MAKE")
+		    (cd "$LOCAL_REPO" && run-in-docker make)
+		    pressAnyKey
+	    ;;
+	    "$MAKE_CLEAN")
+		    (cd "$LOCAL_REPO" && run-in-docker make clean)
+		    pressAnyKey
+	    ;;
+	    "$MAKE_DISTCLEAN")
+		    (cd "$LOCAL_REPO" && run-in-docker make distclean)
+		    pressAnyKey
+	    ;;
+	    "$DOCKER_PULL")
+		    docker pull "$IMAGE"
+		    pressAnyKey
 
-		;;
-		"$CONFIGURATION")
-			configSubMenu
-		;;
-	esac
-done
+	    ;;
+	    "$CONFIGURATION")
+		    configSubMenu
+	    ;;
+    esac
+}
 
+main() {
+    while :; do
+	    value=()
+	    [ -d "$LOCAL_REPO" ] || value+=("$CLONE_REPO" "Creates a new clone of the Freetz-NG github repository.")
+	    [ -d "$LOCAL_REPO" ] && value+=("$PULL_REPO" "Updates the local Freetz-NG clone.")
+	    [ -d "$LOCAL_REPO" ] && value+=("$MAKE_CONFIG" "Configure the firmware (\"make menuconfig\").")
+	    [ -r "$LOCAL_REPO/.config" ] && value+=("$MAKE" "Build the firmware (\"make\").")
+	    [ -d "$LOCAL_REPO" ] && value+=("$MAKE_CLEAN" "Remove unpacked images and some cache files (\"make clean\").")
+	    [ -d "$LOCAL_REPO" ] && value+=("$MAKE_DISTCLEAN" "Clean everything except the download directory (\"make distclean\").")
+	    value+=("$CONFIGURATION" "Config/Tweak this tool.")
+
+	    CHOICE=$(whiptail --title "Freetz-NG build menu" --menu "Main Menu" 15 98 6 "${value[@]}" 3>&1 1>&2 2>&3)
+	    [ "$?" -eq 0 ] || exit
+	    mainChoice $CHOICE
+    done
+}
+
+[ $# -eq 0 ] && main || "$1"
